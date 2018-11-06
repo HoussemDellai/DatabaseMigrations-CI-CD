3c0eec20c2519a9e63bf9ccb222851d788ecdfc5
Houssem Dellai <houssem.dellai@live.com>
2018-11-06 17:49:45 +0100

    Added Product model and controller

---
+++ WebApp/Data/ProductsContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.Models
+{
+    public class ProductsContext : DbContext
+    {
+        public ProductsContext (DbContextOptions<ProductsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Product { get; set; }
+    }
+}
--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
 
 namespace WebApp
 {
@@ -33,6 +35,9 @@ namespace WebApp
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ProductsContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ProductsContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
