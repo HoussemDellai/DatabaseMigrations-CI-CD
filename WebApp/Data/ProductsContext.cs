412bed3dc81bc274d96773f113283da319567f06
Houssem Dellai <houssem.dellai@live.com>
2018-11-06 17:53:29 +0100

    minor changes

--- WebApp/Data/ProductsContext.cs
+++ WebApp/Data/ProductsContext.cs
@@ -7,6 +7,7 @@ namespace WebApp.Models
         public ProductsContext (DbContextOptions<ProductsContext> options)
             : base(options)
         {
+            //Database.Migrate();
         }
 
         public DbSet<Product> Product { get; set; }
